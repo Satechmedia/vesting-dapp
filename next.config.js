2d58912db80aef1ff74c5a1ad3572060d62157ac
Satechmedia <88564875+Satechmedia@users.noreply.github.com>
2022-05-24 00:04:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ module.exports = {
     ],
   },
 
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
   // typescript: {
   //   // !! WARN !!
